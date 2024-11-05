37bd407ec35dcb87ea58a8450f8f16abd1ebd899
Ajay Cyriac <98336671+indianaranger@users.noreply.github.com>
2024-11-05 16:04:00 +0530

    Create aboutUs.php

---
+++ blogger/aboutUs/aboutUs.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>About Us Section</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css">
+    <?php require "../partials/links.php" ?>
+	<link rel="stylesheet" type="text/css" href="../static/css/aboutUs.css">
+
+</head>	
+<body>
+    <?php 
+    require "../partials/sideBar.php";
+    ?>
+	<div class="section">
+		<div class="container">
+			<div class="content-section">
+				<h1 class="title">About us</h1>
+				<p class="text">
+				Welcome to a space where your voice takes center stage. Share your passions, insights, and creativity with a community eager to listen, explore, and connect. Here, you’re free to express without limits<span class="dots">.........</span><span class="more-text">whether through storytelling, art, or ideas. Write and read the content that resonates with you, showcase your unique perspectives, and join a platform that celebrates authenticity and individuality. This is your place to inspire, connect, and let your voice shine.
+              <button class="read-more-btn">Read more</button>
+				<div class="social">
+					<a href=""><i class="fab fa-facebook-f"></i></a>
+					<a href=""><i class="fab fa-twitter"></i></a>
+					<a href=""><i class="fab fa-instagram"></i></a>
+				</div>
+			</div>
+			<div class="image-section">
+				<img src="../static/images/shuja.png"/>
+			</div>
+		</div>
+	</div>
+
+
+	<section class="team-section">
+		<div class="container">
+			<div class="row">
+				<div class="section-title">
+					<h1>Our Team</h1>
+					<p>Developers and current managing team,for detailed information of individual visit thier socials:</p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="team-items">
+					 <div class="item">
+						 <img src="../static/images/ajay.jpg" alt="team" />
+						 <div class="inner">
+							 <div class="info">
+								  <h5>Ajay Cyriac</h5>
+								  <p>Front-end Developer</p>
+								  <div class="social-links">
+									<a href="https://github.com/indianaranger"><i class="fab fa-github"></i></a>
+									<a href="https://www.linkedin.com/in/ajay-cyriac-b8b501234/"><i class="fab fa-linkedin"></i></a>
+									<a href=""><i class="fas fa-globe"></i></a>
+								</div>
+							 </div>
+						 </div>
+					 </div>
+					 <div class="item">
+						 <img src="images/" alt="team" />
+						 <div class="inner">
+							 <div class="info">
+								  <h5>	Alan Peter</h5>
+								  <p>Back-end Developer</p>
+								  <div class="social-links">
+									<a href=""><i class="fab fa-github"></i></a>
+									<a href=""><i class="fab fa-linkedin"></i></a>
+									<a href=""><i class="fas fa-globe"></i></a>
+								  </div>
+							 </div>
+						 </div>
+					 </div>
+					 <div class="item">
+						 <img src="../static/images/" alt="team" />
+						 <div class="inner">
+							 <div class="info">
+								  <h5>Edwin Rajesh</h5>
+								  <p>Database management</p>
+								  <div class="social-links">
+									<a href=""><i class="fab fa-github"></i></a>
+									<a href=""><i class="fab fa-linkedin"></i></a>
+									<a href=""><i class="fas fa-globe"></i></a>
+								  </div>
+							 </div>
+						 </div>
+					 </div>
+					 <div class="item">
+						 <img src="../static/images/" alt="team" />
+						 <div class="inner">
+							 <div class="info">
+								  <h5>Amal Anilkumar</h5>
+								  <p>Content-Designer</p>
+								  <div class="social-links">
+									<a href=""><i class="fab fa-github"></i></a>
+									<a href=""><i class="fab fa-linkedin"></i></a>
+									<a href=""><i class="fas fa-globe"></i></a>
+								  </div>
+							 </div>
+						 </div>
+					 </div>
+					 <div class="item">
+						 <img src="../static/images/" alt="team" />
+						 <div class="inner">
+							 <div class="info">
+								  <h5>Tobin Thomas</h5>
+								  <p>Database management</p>
+								  <div class="social-links">
+									<a href=""><i class="fab fa-github"></i></a>
+									<a href=""><i class="fab fa-linkedin"></i></a>
+									<a href=""><i class="fas fa-globe"></i></a>
+								  </div>
+							 </div>
+						 </div>
+					 </div>
+					 <div class="item">
+						 <img src="../static/images/" alt="team" />
+						 <div class="inner">
+							 <div class="info">
+								  <h5>Aman Kapoor</h5>
+								  <p>Database management</p>
+								  <div class="social-links">
+									<a href=""><i class="fab fa-github"></i></a>
+									<a href=""><i class="fab fa-linkedin"></i></a>
+									<a href=""><i class="fas fa-globe"></i></a>
+								  </div>
+							 </div>
+						 </div>
+					 </div>
+					 <div class="item">
+						 <img src="../static/images/" alt="team" />
+						 <div class="inner">
+							 <div class="info">
+								  <h5>Jithmon P Cherian</h5>
+								  <p>Database management</p>
+								  <div class="social-links">
+									<a href=""><i class="fab fa-github"></i></a>
+									<a href=""><i class="fab fa-linkedin"></i></a>
+									<a href=""><i class="fas fa-globe"></i></a>
+								  </div>
+							 </div>
+						 </div>
+					 </div>
+				</div>
+			</div>
+		</div>
+	<script src="../static/js/aboutUs.js"></script>
+</body>
+</html>
+
+
+
+
+
